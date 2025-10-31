b1f24661a159d8026202daca579b3c63052104c8
Saidxdd <svidalramirz@gmail.com>
2025-10-31 10:48:26 -0600

    ahhhhh prueba

---
+++ Source/Faro/HUDFaro.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/HUD.h"
+#include "HUDFaro.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class FARO_API AHUDFaro : public AHUD
+{
+	GENERATED_BODY()
+	
+};
